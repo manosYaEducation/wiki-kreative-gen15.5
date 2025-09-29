758902e61d33c533a912e49b69e84bbed5a88f60
tdelacarrera <tdelacarrera@protonmail.com>
2025-09-29 09:51:47 -0300

    Se ha separado el header en su propio componente

---
+++ views/components/header.php
@@ -0,0 +1,24 @@
+    <!-- Header -->
+    <header class="header">
+        <nav>
+            <div class="nav-left">
+            <a href="index" class="back-to-site">
+			    <i class="fas fa-arrow-left"></i> Volver a la Wiki
+            </a>
+                <img src="../assets/img/kreativenofondo.png" alt="Icono" class="icon">
+            </div>
+            <div class="navbar" class="back-button">
+                <ul class="headernav">
+                    <li><a class="button-53" href="../frontend/ordenes-servicios.html">Ordenes Servicios</a></li>
+                    <li><a class="button-53" href="../frontend/proyectos-admin.html">Proyectos</a></li>
+                    <li><a class="button-53" href="../frontend/index-admin.html">Panel de Administración</a></li>
+                </ul>
+            </div>
+            <div class="hamburger" id="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+		<button class="theme-toggle" onclick="toggleTheme()">🌙</button>
+    </header>
--- views/detail.php
+++ views/detail.php
@@ -17,30 +17,7 @@
         rel="stylesheet">
 </head>
 <body class="detail-page">
-    <!-- Header -->
-    <header class="header">
-        <nav>
-            <div class="nav-left">
-            <a href="index" class="back-to-site">
-			    <i class="fas fa-arrow-left"></i> Volver a la Wiki
-            </a>
-                <img src="../assets/img/kreativenofondo.png" alt="Icono" class="icon">
-            </div>
-            <div class="navbar" class="back-button">
-                <ul class="headernav">
-                    <li><a class="button-53" href="../frontend/ordenes-servicios.html">Ordenes Servicios</a></li>
-                    <li><a class="button-53" href="../frontend/proyectos-admin.html">Proyectos</a></li>
-                    <li><a class="button-53" href="../frontend/index-admin.html">Panel de Administración</a></li>
-                </ul>
-            </div>
-            <div class="hamburger" id="hamburger">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </nav>
-		<button class="theme-toggle" onclick="toggleTheme()">🌙</button>
-    </header>
+    <?php require_once("components/header.php"); ?>
     <section class="ambientes-section" id="enlaces">
        
             <div class="enlaces">
